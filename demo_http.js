74816e91a3f4a5f36e1eaed1c0f259ea2449ddf6
SquareRuthZ <89763888+SquareRuthZ@users.noreply.github.com>
2024-07-31 15:54:52 +0700

    Init NodeJS

---
+++ demo_http.js
@@ -0,0 +1,11 @@
+let http = require('http');
+let url = require('url');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+/*Use the url module to turn the querystring into an object:*/
+var q = url.parse(req.url, true).query;
+/*Return the year and month from the query object:*/
+var txtname = 'Name:' + q.name;
+var txtdate = '<br/>Login Date:' + q.month + q.day+ ", " + q.year;
+res.end(txtname+txtdate);
+}).listen(9999);
